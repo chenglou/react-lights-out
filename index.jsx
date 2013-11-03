d25a623672de190d2d350d48265acdd8d258bff2
Cheng Lou <chenglou92@gmail.com>
2013-11-02 23:04:15 -0400

    init js anim try

--- index.jsx
+++ index.jsx
@@ -24,10 +24,111 @@ var boards = [
   ]
 ];
 
+// boards = [
+//   [[1]]
+// ]
+
 var TransitionGroup = React.addons.TransitionGroup;
 var classSet = React.addons.classSet;
 
+
+function easeOut3(curX, destX, initY, destY) {
+  return Math.pow(curX / destX, 1/3) * (destY - initY) + initY;
+}
+
+function linear(curX, destX, initY, destY) {
+  return curX / destX * (destY - initY) + initY;
+}
+
+var Apple = {
+  animate: function(initState, transitionState, doneFunc) {
+    initState(doneFunc);
+  }
+};
+
 var Switch = React.createClass({
+  mixins: [Apple],
+
+  isTransitioning: false,
+
+  doUpdateTransition: false,
+
+  getInitialState: function() {
+    return {
+      scale: 0
+    }
+  },
+
+  componentDidMount: function() {
+    this.isTransitioning = true;
+
+    var totalDuration = 300;
+    var initObj = {
+      scale: 0
+    }
+    var finalObj = {
+      scale: 1
+    }
+
+    // this.applefy(initObj, finalObj);
+    this.enterAnim(function() {
+      this.isTransitioning = false;
+    }.bind(this));
+  },
+
+  componentWillReceiveProps: function() {
+    this.doUpdateTransition = true;
+  },
+
+  componentDidUpdate: function() {
+    console.log(this.props.done);
+    if (this.isTransitioning || this.props.done) {
+      return;
+    }
+
+    if (this.doUpdateTransition) {
+      this.doUpdateTransition = false;
+      this.updateAnim(function() {
+        // console.log('done');
+      }.bind(this));
+    }
+  },
+
+  updateAnim: function(done) {
+    this.setState({scale: 0}, function() {
+      setTimeout(function() {
+        this.enterActiveAnim(0, done);
+      }.bind(this), (this.props.posX + this.props.posY) * 35);
+    }.bind(this));
+  },
+
+  enterAnim: function(done) {
+    setTimeout(function() {
+      this.setState({scale: 0}, function() {
+        this.enterActiveAnim(0, done);
+      }.bind(this));
+    }.bind(this), (this.props.posX + this.props.posY) * 35);
+  },
+
+  enterActiveAnim: function(framesDone, done) {
+    var totalDuration = 300;
+    var totalFrameBudget = totalDuration / (1000 / 60);
+    var initScale = 0;
+    var finalScale = 1;
+    var curScale = easeOut3(framesDone, totalFrameBudget, initScale, finalScale);
+
+    if (framesDone >= totalFrameBudget) {
+      this.setState({scale: finalScale}, done);
+      return;
+    }
+
+    requestAnimationFrame(function() {
+      this.setState({scale: curScale}, function() {
+        this.enterActiveAnim(framesDone + 1, done);
+      }.bind(this));
+    }.bind(this));
+  },
+
   render: function() {
     // got the props from parent (LightsOut)
     var classes = {
@@ -36,8 +137,11 @@ var Switch = React.createClass({
       'switch-on': this.props.isOn,
       'switch-off': !this.props.isOn
     }
-
-    return <div className={classSet(classes)} onClick={this.props.onClick} />
+    var style = {
+      transform: 'scale(' + this.state.scale + ')',
+      WebkitTransform: 'scale(' + this.state.scale + ')'
+    };
+    return <div className={classSet(classes)} style={style} onClick={this.props.onClick} />
   }
 });
 
@@ -79,21 +183,20 @@ var LightsOut = React.createClass({
 
     // setState is asynchronous. Pass a callback that verifies if all the lights
     // are on; if so, create new game
-    this.setState({board: this.state.board}, function() {
-      var done = this.state.board.every(function(row) {
-        return row.every(function(cell) {
-          return !!cell;
-        });
+    var done = this.state.board.every(function(row) {
+      return row.every(function(cell) {
+        return !!cell;
       });
+    });
 
+    this.setState({board: this.state.board, done: done}, function() {
       if (done) {
-        this.setState({done: true});
         setTimeout(function() {
           this.setState({
             board: this.getNewRandomBoard(),
             done: false
           });
-        }.bind(this), 1500);
+        }.bind(this), 500);
       }
     }.bind(this));
   },
@@ -113,7 +216,7 @@ var LightsOut = React.createClass({
           this.state.board.map(function(row, i) {
             return (
               // `TransitionGroup` defaults to a `span` wrapper. We want a `div`
-              <TransitionGroup
+              <div
                 transitionName="switch"
                 component={React.DOM.div}
               >
@@ -125,11 +228,13 @@ var LightsOut = React.createClass({
                         isOn={!!cell}
                         done={this.state.done}
                         onClick={this.handleSwitchClick.bind(this, i, j)}
+                        posX={i}
+                        posY={j}
                       />
                     )
                   }, this)
                 }
-              </TransitionGroup>
+              </div>
             )
           }, this)
         }
